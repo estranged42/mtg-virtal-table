f1aa0a07b70619efe17446ef7c58bc684d3f2f49
Mark Fischer <estranged@mac.com>
2020-10-18 22:00:14 -0700

    Table and Player skeletons

--- src/App.vue
+++ src/App.vue
@@ -27,30 +27,22 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <Table/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Table from './components/Table';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Table,
   },
 
   data: () => ({
---
+++ src/components/Table.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Player v-for="p in players" :key="p.id"/>
+    </div>
+</template>
+
+
+<script>
+import Player from './Player';
+
+export default {
+    components: {
+        Player,
+    },
+    data: () => ({
+        players: [
+            {id: 1},
+            {id: 2}
+        ]
+    })
+}
+</script>
