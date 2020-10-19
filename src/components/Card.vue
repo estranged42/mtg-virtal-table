54bc63202059be219cd0ee7face3eac5d73b1dea
Mark Fischer <estranged@mac.com>
2020-10-19 14:44:40 -0700

    Added mana cost placeholder

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,34 +1,72 @@
 <template>
     <v-card
         width="300"
-        outlined=true
+        outlined
         class="card_slug"
     >
-        <v-card-title
-            class="card_title"
-            v-text="carddata.name"
-        ></v-card-title>
-
+        <v-container>
+            <v-row
+                no-gutters
+                align="center"
+                align-content="center"
+            >
+                <v-col>
+                    <v-card-title
+                        class="card_title"
+                        v-text="carddata.name"
+                    ></v-card-title>
+                </v-col>
+                <v-spacer/>
+                <v-col class="manacost_container">
+                    <ManaCost/>
+                    <ManaCost/>
+                </v-col>
+            </v-row>
+        </v-container>
+        
     </v-card>
     
 </template>
 
 <script>
+import ManaCost from './ManaCost';
 
 export default {
-    props: ['carddata']
+    props: ['carddata'],
+    components: {
+        ManaCost,
+    }
 }
 </script>
 
 <style>
 
 .v-autocomplete__content .v-list-item {
-    padding: 5px;
+    padding: 0 5px;
 }
 
-.card_title {
+.card_slug .card_title {
     font-size: 1rem;
+    padding: 0;
+}
+
+.card_slug .container {
+    padding: 0;
+}
+
+.card_slug .row {
     padding: 2px 8px;
+}
+
+.card_slug .col {
+    padding: 0;
+    width: inherit;
+}
+
+.card_slug .manacost_container {
+    flex-grow: unset;
+    flex-basis: unset;
+    line-height: 1;
 }
 
 </style>
---
+++ src/components/ManaCost.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mana_cost"></div>
+</template>
+
+<script>
+
+export default {
+    props: ['cost']
+}
+</script>
+
+<style>
+
+.mana_cost {
+    height: 16px;
+    width: 16px;
+    background: url('/images/mana_cost_sprites.gif') no-repeat;
+    display: inline-block;
+    padding: 1px;
+}
+
+</style>
