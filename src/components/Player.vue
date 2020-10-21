bf9ecae6efc9b6ac265c30b3eaf0d186dc11e107
Mark Fischer <estranged@mac.com>
2020-10-20 20:21:31 -0700

    Added easy-dnd module, started drag and drop testing

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,12 +1,33 @@
 <template>
-    <div>
-        <p>I'm a player!</p>
-    </div>
+    <v-card
+        class="mx-auto player-card"
+        max-width="800"
+        min-width="400"
+    >
+        <v-toolbar
+            color="indigo"
+            dark
+        >
+            <v-toolbar-title>{{ player.name }}</v-toolbar-title>
+        </v-toolbar>
+        <v-container fluid>
+            <v-row dense>
+            </v-row>
+        </v-container>
+    </v-card>
     
 </template>
 
 <script>
 export default {
-    
+    props: ['player'],
 }
 </script>
+
+<style lang="scss" scoped>
+
+.player-card {
+    margin: 15px;
+}
+
+</style>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,22 +1,50 @@
 <template>
-    <div>
-        <Player v-for="p in players" :key="p.id"/>
-    </div>
+    <drop-list
+        :items="players"
+        class="players"
+        @insert="onInsert"
+        @reorder="onReorder"
+    >
+        <template v-slot:item="{item}">
+            <drag class="item" :key="item.id">
+                <Player :player="item"/>
+            </drag>
+        </template>
+    </drop-list>
 </template>
 
 
 <script>
+import { Drag, DropList } from "vue-easy-dnd";
 import Player from './Player';
 
 export default {
     components: {
         Player,
+        Drag,
+        DropList
     },
     data: () => ({
         players: [
-            {id: 1},
-            {id: 2}
+            {id: 2, name: "Eric"},
+            {id: 1, name: "Mark"},
         ]
-    })
+    }),
+    methods: {
+        onInsert(event) {
+            this.players.splice(event.index, 0, event.data);
+        },
+        onReorder(event) {
+            event.apply(this.players)
+        }
+    }
 }
 </script>
+
+<style scoped lang="scss">
+
+.players {
+    margin: 5px;
+}
+
+</style>
