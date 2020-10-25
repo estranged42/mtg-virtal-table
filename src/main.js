cb9ab68e9a3a3b7ad2efda89ecb6ba38f53ae167
Mark Fischer <estranged@mac.com>
2020-10-24 19:21:50 -0700

    Global card ID generation

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
+let next_card_id = 1
+Vue.prototype.getCardId = function() {
+  let id = next_card_id
+  next_card_id = id + 1
+  return id
+}
+
 new Vue({
   vuetify,
   render: h => h(App)
