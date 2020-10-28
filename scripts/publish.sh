2bf5fddac9ba60bedba9fe20c83474e7f12e6e73
Mark Fischer <estranged@mac.com>
2020-10-27 17:37:52 -0700

    Trigger cloudfront invalidation on publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -11,3 +11,18 @@ if [ -d "$DISTRIBUTION_FOLDER" ]; then
 else 
     echo "$DISTRIBUTION_FOLDER does not exist. Run `yarn build` first"
 fi
+
+
+DISTRIBUTION_ID="$(get_stack_output $FRONTEND_STACK_NAME CloudFrontDistribution)"
+
+INVALIDATION_ID=$(aws cloudfront create-invalidation \
+    --query "Invalidation.Id" --output text \
+    --distribution-id $DISTRIBUTION_ID \
+    --paths "/*" \
+    2>&1)
+
+echo "Waiting for invalidation to complete... $INVALIDATION_ID"
+
+aws cloudfront wait invalidation-completed \
+    --distribution-id $DISTRIBUTION_ID \
+    --id $INVALIDATION_ID
